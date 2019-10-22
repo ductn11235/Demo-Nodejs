ca0fbdd612e12b3e5acb270d7fecfa103e1889e0
DuctnNetwork <trannhuduc531998@gmail.com>
2019-10-22 20:13:00 +0700

    Add route file

---
+++ routes/user.route.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var controller = require('../controllers/user.controller');
+
+
+router.get('/', controller.index);
+router.get('/search', controller.search);
+router.get('/create', controller.getCreate);
+router.get('/:id', controller.get);
+router.post('/create', controller.postCreate);
+
+
+module.exports = router;
