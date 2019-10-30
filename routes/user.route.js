1d66f703a162ba3efa3b9ca2780059a875317ab5
DuctnNetwork <trannhuduc531998@gmail.com>
2019-10-30 08:35:07 +0700

    Add validate file

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/user.controller');
+var validate = require('../validate/user.validate');
 
 
 router.get('/', controller.index);
 router.get('/search', controller.search);
 router.get('/create', controller.getCreate);
 router.get('/:id', controller.get);
-router.post('/create', controller.postCreate);
+router.get('/cookie', function(req, res, next){
+    res.cookie('user-id', 123);
+    res.send('hello');
+});
+router.post('/create', validate.postCreate, controller.postCreate);
+
 
 
 module.exports = router;
